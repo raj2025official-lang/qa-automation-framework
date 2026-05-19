9eff6d70c7f5223688c1e385ec3bbe68dc4b4b2e
Shakya <your_email@gmail.com>
2026-05-19 18:42:22 +0530

    updated conftest for github actions

--- conftest.py
+++ conftest.py
@@ -1,20 +1,23 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.options import Options
 import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
 
 @pytest.fixture
 def driver():
 
     chrome_options = Options()
 
+    # GitHub Actions / Headless Chrome Fix
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--window-size=1920,1080")
 
     driver = webdriver.Chrome(options=chrome_options)
 
     driver.maximize_window()
+    driver.implicitly_wait(10)
 
     yield driver
 
